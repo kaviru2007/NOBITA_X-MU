9d412cca68508792b6d2e5cdd1037e76edea4047
Jai Singh <88092614+Jaisingh007@users.noreply.github.com>
2023-06-01 13:22:29 +0530

    config.py

--- config.py
+++ config.py
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "http://telegra.ph/file/b6971bb4dea74d756437b.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/991622df1696c9b5c5a4f.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/56d1760224589ee370186.jpg",
+    "https://te.legra.ph/file/991622df1696c9b5c5a4f.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
